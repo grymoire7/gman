b1bb6254b69019d4596ff33cdca9d04e4f26c690
Tracy Atteberry <tracy@magicbydesign.com>
2014-02-12 15:56:56 -0600

    began docopt usage

--- source/src/gman/config.go
+++ source/src/gman/config.go
@@ -5,64 +5,74 @@
 package main
 
 import (
-    "encoding/json"   // for reading config file
-    "io/ioutil"       // for reading files and logging
-    "os/user"         // for finding user home directory
-    "log"             // for debug logging
+    "docopt"        // for command line options parsing
+    "encoding/json" // for reading config file
+    "io/ioutil"     // for reading files and logging
+    "log"           // for debug logging
+    "os/user"       // for finding user home directory
 )
 
-// TODO: use a better type for GManPathMap
-type ConfigObject struct {
-    Help  bool                   // Show help (gman help page)
-    Debug bool
-    Man   bool
-    Section  string
-    HttpPort string
-    SectionSearchOrder string
-    GManPath    []string
-    GManPathMap []string
+var GMAN_VERSION = "0.1rc"
+
+func loadJSONConfig(filename string) (map[string]interface{}, error) {
+    var result map[string]interface{}
+    file, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return result, err
+    }
+    err = json.Unmarshal(file, &result)
+    return result, err
 }
 
-// Define options with some defaults
-var config = ConfigObject {
-    Help:     false,
-    Debug:    false,
-    Man:      false,
-    Section:  "",
-    HttpPort: "8088",
-    SectionSearchOrder: "1 n l 8 3 2 3posix 3pm 3perl 5 4 9 6 7",
-    GManPath: []string{"", ""},
-    GManPathMap: []string{"", ""},
+// merge combines two maps.
+// truthiness takes priority over falsiness
+// mapA takes priority over mapB
+func merge(mapA, mapB map[string]interface{}) map[string]interface{} {
+    result := make(map[string]interface{})
+    for k, v := range mapA {
+        result[k] = v
+    }
+    for k, v := range mapB {
+        if _, ok := result[k]; !ok || result[k] == nil || result[k] == false {
+            result[k] = v
+        }
+    }
+    return result
 }
 
-// TODO: We probably need to return extra command line arguments
-// as an array of strings.
-func readConfig() ConfigObject {
+func readConfig() map[string]interface{} {
+    usage := `GMan
+
+Usage:
+  gman [-d | --debug] [--color] [-s <docsection>] <page>
+  gman (-b | --browse) [(-p <port> | --port <port>)]
+  gman (-h | --help | -V | --version )
+
+Options:
+  -h --help                Show this help.
+  -d --debug               Print debug information.
+  --color                  Use color text in terminal.
+  -s <docsection>          Print document section.
+  -p <port> --port <port>  Specifiy port for web server.
+  -V --version             Show version.`
 
     // get user directory
     usr, err := user.Current()
     if err != nil {
         log.Println("Error file user home directory: ", err)
-        return config
+        return nil
     }
     log.Println("User homedir: ", usr.HomeDir)
 
-    // TODO: Check for config file in /etc/gmanrc
-    // DONE: Check for config file in $HOME/.gmanrc
-    // TODO: If config.Man then read /etc/manpath.config
-    file, err := ioutil.ReadFile(usr.HomeDir + "/.gmanrc")
-    if err != nil {
-        // If we didn't find a config file in the user's home directory
-        // then we'll look for the gmanrc file colocated with the executable.
-        file, err = ioutil.ReadFile("gmanrc")
-        if err != nil {
-            log.Println("Error reading .gmanrc file: ", err)
-        }
-        return config
-    }
+    binConfig, _ := loadJSONConfig("gmanrc")
+    etcConfig, _ := loadJSONConfig("/etc/gman.conf")
+    homeConfig, _ := loadJSONConfig(usr.HomeDir + "/.gmanrc")
+    arguments, _ := docopt.Parse(usage, nil, true, GMAN_VERSION, false)
 
-    json.Unmarshal(file, &config)
-    return config
+    result := merge(arguments, merge(homeConfig, merge(etcConfig, binConfig)))
+
+    // config.Section = result["this"].(string)
+    // json.Unmarshal(file, &config)
+    // return config
+    return result
 }
-
-
--- source/src/gman/gman.go
+++ source/src/gman/gman.go
@@ -18,21 +18,14 @@
 package main
 
 import (
-    "flag"            // for command line parameters
-    "io/ioutil"       // for reading files and logging
-    "os"              // for local file access
-    "log"             // for debug logging
-    "blackfriday"     // markdown parser
-    "fmt"             // for printing runtime errors
-    // "net/http"        // for creating an http web service
-    // "net/url"         // for url escaping
-    // "open"            // for launching the web browser
-    // "os/exec"         // for executing conversions
-    // "path"            // for local file system access
-    // "path/filepath"   // for local file system access
-    // "regexp"          // for file parsing
-    // "sort"            // for sorting the file list
-    // "strings"         // for string manipulation
+    "blackfriday" // markdown parser
+    "fmt"         // for printing runtime errors
+    "io"          // for piping through pager
+    "io/ioutil"   // for reading files and logging
+    "log"         // for debug logging
+    "os"          // for local file access
+    "os/exec"     // for piping through pager
+    "strings"     // for string manipulation
 )
 
 func init() {
@@ -40,56 +33,37 @@ func init() {
 
 func main() {
     // Get configuration options from .gmanrc first.
-    config := readConfig()
-
-    // TODO: Command line options handling may soon deserve it's own source file.
-    // Override config values with options from the command line.
-    // We do flags this way so we can supplement os.Args with
-    // arguments from the .gmanrc file (config.Clopts).
-    f := flag.NewFlagSet("gman", flag.ExitOnError)
-    f.BoolVar(  &config.Debug,   "d",   config.Debug,   "turn on debug info")
-    f.BoolVar(  &config.Man,     "man", config.Man,     "use man pages as input")
-    f.StringVar(&config.Section, "s",   config.Section, "which help sections to print")
-
-    // TODO: prepend config.Clopts  to os.Args[1:] for parsing
-    f.Parse(os.Args[1:])
-    // f.Parse([]string{"-d", "-s", "bob"})
+    opts := readConfig()
 
     // Discard logging messages if not in debug mode.
-    if config.Debug {
-        log.Println("Debug on");
+    if debug, ok := opts["--debug"]; ok && debug.(bool) {
+        log.Println("Debug on")
     } else {
-        log.Println("Debug off");
+        log.Println("Debug off")
         log.SetOutput(ioutil.Discard)
     }
 
+    for k, v := range opts {
+        if !strings.HasPrefix(k, "_") {
+            log.Printf("map: %s => %v\n", k, v)
+        }
+    }
+    // TODO: Should be more robust. Should search a path collection and allow
+    // missing os and lang dirs.
+    page := opts["<page>"].(string)
+    gmanpath := opts["gmanpath"].(string)
+    pagepath := gmanpath + "/linux/en/gman1/" + page + ".1.md"
+
     var input []byte
     var err error
-    var args = f.Args()
-    // TODO: look for gman file in gmanpath
+
     // TODO: support .gz
-
-    switch len(args) {
-    case 0:
-        if input, err = ioutil.ReadAll(os.Stdin); err != nil {
-            fmt.Fprintln(os.Stderr, "Error reading from Stdin:", err)
-            os.Exit(-1)
-        }
-    case 1,2:
-        if input, err = ioutil.ReadFile(args[0]); err != nil {
-            fmt.Fprintln(os.Stderr, "Error reading from", args[0], ":", err)
-            os.Exit(-1)
-        }
-    default:
-        fmt.Println("Too many arguments (", len(args), ").")
-        os.Exit(-1);
+    if input, err = ioutil.ReadFile(pagepath); err != nil {
+        fmt.Fprintln(os.Stderr, "Error reading from", pagepath, ":", err)
+        os.Exit(-1)
     }
 
-    if len(args) > 0 {
-        log.Println("In from:", args[0])
-    }
-
-	extensions := 0
+    extensions := 0
     extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
     extensions |= blackfriday.EXTENSION_TABLES
     extensions |= blackfriday.EXTENSION_FENCED_CODE
@@ -98,26 +72,30 @@ func main() {
     extensions |= blackfriday.EXTENSION_SPACE_HEADERS
 
     renderer := blackfriday.TerminalRenderer(0)
-    // output := blackfriday.MarkdownCommon(input)
     output := blackfriday.Markdown(input, renderer, extensions)
 
-   	// output the result
-    var out *os.File
-    if len(args) == 2 {
-        if out, err = os.Create(args[1]); err != nil {
-            fmt.Fprintf(os.Stderr, "Error creating %s: %v", args[1], err)
-            os.Exit(-1)
-        }
-        defer out.Close()
-    } else {
-        out = os.Stdout
-    }
+    // declare the pager
+    cmd := exec.Command("less", "-R")
+    // create a blocking pipe
+    r, stdin := io.Pipe()
+    // Set the i/o's
+    cmd.Stdin = r
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-    if _, err = out.Write(output); err != nil {
-        fmt.Fprintln(os.Stderr, "Error writing output:", err)
-        os.Exit(-1)
-    }
+    // Create a blocking chan, Run the pager and unblock once it is finished
+    c := make(chan struct{})
+    go func() {
+        defer close(c)
+        cmd.Run()
+    }()
 
-    log.Printf("Section: %s\n", config.Section)
+    // Pass output to the pipe
+    fmt.Fprintf(stdin, string(output))
+
+    // Close stdin (allows pager to exit)
+    stdin.Close()
+
+    // Wait for the pager to be finished
+    <-c
 }
-
